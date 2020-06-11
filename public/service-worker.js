6d45b31cfd0979dc3155d0e12534d3b07623f737
Paulo Teixeira <pauloteixeirajr@outlook.com>
2020-06-11 20:08:22 +0100

    caching the app shell

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,23 @@
 self.addEventListener('install', function (event) {
   console.log('[Service worker] Install event...', event);
+  event.waitUntil(
+    caches.open('static').then(function (cache) {
+      // Cache all url requests
+      cache.addAll([
+        '/',
+        '/index.html',
+        '/src/js/app.js',
+        '/src/js/feed.js',
+        '/src/js/material.min.js',
+        '/src/css/app.css',
+        '/src/css/feed.css',
+        '/src/images/main-image.jpg',
+        'https://fonts.googleapis.com/css?family=Roboto:400,700',
+        'https://fonts.googleapis.com/icon?family=Material+Icons',
+        'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css',
+      ]);
+    })
+  );
 });
 
 self.addEventListener('activate', function (event) {
@@ -8,5 +26,12 @@ self.addEventListener('activate', function (event) {
 });
 
 self.addEventListener('fetch', function (event) {
-  console.log('[Service worker] Fetch event...', event);
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      // Response will be null if cache is not available
+      if (response) return response;
+
+      return fetch(event.request);
+    })
+  );
 });
