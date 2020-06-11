87c9682d1986462f039b71258a72421db327448f
Paulo Teixeira <pauloteixeirajr@outlook.com>
2020-06-11 18:59:55 +0100

    adding dummy card

--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -3,21 +3,23 @@ var createPostArea = document.querySelector('#create-post');
 var closeCreatePostModalButton = document.querySelector(
   '#close-create-post-modal-btn'
 );
+var sharedMomentsArea = document.querySelector('#shared-moments');
 
 function openCreatePostModal() {
   createPostArea.style.display = 'block';
   if (deferredPrompt) {
     deferredPrompt.prompt();
 
-    deferredPrompt.userChoice.then(function (result) {
-      console.log(result.outcome);
+    deferredPrompt.userChoice.then(function (choiceResult) {
+      console.log(choiceResult.outcome);
 
-      if (result.outcome === 'dimissed') {
+      if (choiceResult.outcome === 'dismissed') {
         console.log('User cancelled installation');
       } else {
         console.log('User added to home screen');
       }
     });
+
     deferredPrompt = null;
   }
 }
@@ -29,3 +31,35 @@ function closeCreatePostModal() {
 shareImageButton.addEventListener('click', openCreatePostModal);
 
 closeCreatePostModalButton.addEventListener('click', closeCreatePostModal);
+
+function createCard() {
+  var cardWrapper = document.createElement('div');
+  cardWrapper.className = 'shared-moment-card mdl-card mdl-shadow--2dp';
+  cardWrapper.style.marginLeft = '50%';
+  cardWrapper.style.transform = 'translate(-50%, 0)';
+  var cardTitle = document.createElement('div');
+  cardTitle.className = 'mdl-card__title';
+  cardTitle.style.backgroundImage = 'url("/src/images/sf-boat.jpg")';
+  cardTitle.style.backgroundSize = 'cover';
+  cardTitle.style.height = '180px';
+  cardWrapper.appendChild(cardTitle);
+  var cardTitleTextElement = document.createElement('h2');
+  cardTitleTextElement.className = 'mdl-card__title-text';
+  cardTitleTextElement.textContent = 'San Francisco Trip';
+  cardTitle.appendChild(cardTitleTextElement);
+  var cardSupportingText = document.createElement('div');
+  cardSupportingText.className = 'mdl-card__supporting-text';
+  cardSupportingText.textContent = 'In San Francisco';
+  cardSupportingText.style.textAlign = 'center';
+  cardWrapper.appendChild(cardSupportingText);
+  componentHandler.upgradeElement(cardWrapper);
+  sharedMomentsArea.appendChild(cardWrapper);
+}
+
+fetch('https://httpbin.org/get')
+  .then(function (res) {
+    return res.json();
+  })
+  .then(function (data) {
+    createCard();
+  });
