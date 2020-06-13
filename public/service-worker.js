4bfebaa3aebd418b1777bbede36cf184ca7974b5
Paulo Teixeira <pauloteixeirajr@outlook.com>
2020-06-13 14:40:54 +0100

    applying cache only for static files

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,25 +1,25 @@
 const CACHE_STATIC = 'static-v2';
 const CACHE_DYNAMIC = 'dynamic-v2';
+const STATIC_FILES = [
+  '/index.html',
+  '/offline.html',
+  '/src/js/app.js',
+  '/src/js/feed.js',
+  '/src/js/material.min.js',
+  '/src/css/app.css',
+  '/src/css/feed.css',
+  '/src/images/main-image.jpg',
+  'https://fonts.googleapis.com/css?family=Roboto:400,700',
+  'https://fonts.googleapis.com/icon?family=Material+Icons',
+  'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css',
+];
 
 self.addEventListener('install', function (event) {
   console.log('[Service worker] Install event...', event);
   event.waitUntil(
     caches.open(CACHE_STATIC).then(function (cache) {
       // Cache all url requests
-      cache.addAll([
-        '/',
-        '/index.html',
-        '/offline.html',
-        '/src/js/app.js',
-        '/src/js/feed.js',
-        '/src/js/material.min.js',
-        '/src/css/app.css',
-        '/src/css/feed.css',
-        '/src/images/main-image.jpg',
-        'https://fonts.googleapis.com/css?family=Roboto:400,700',
-        'https://fonts.googleapis.com/icon?family=Material+Icons',
-        'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css',
-      ]);
+      cache.addAll(['/', ...STATIC_FILES]);
     })
   );
 });
@@ -51,6 +51,13 @@ self.addEventListener('fetch', function (event) {
         });
       })
     );
+  } else if (
+    new RegExp('\\b' + STATIC_FILES.join('\\b|\\b') + '\\b').test(
+      event.request.url
+    )
+  ) {
+    console.log(event.request.url);
+    event.respondWith(caches.match(event.request));
   } else {
     event.respondWith(
       caches.match(event.request).then(function (response) {
