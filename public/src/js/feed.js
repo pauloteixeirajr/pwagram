0335e4a3cc2b6e7c31014ddc454923f3939e9876
Paulo Teixeira <pauloteixeirajr@outlook.com>
2020-06-14 13:20:14 +0100

    adding responsive design

--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -41,13 +41,10 @@ function clearCards() {
 function createCard(data) {
   let cardWrapper = document.createElement('div');
   cardWrapper.className = 'shared-moment-card mdl-card mdl-shadow--2dp';
-  cardWrapper.style.marginLeft = '50%';
-  cardWrapper.style.transform = 'translate(-50%, 0)';
   let cardTitle = document.createElement('div');
   cardTitle.className = 'mdl-card__title';
   cardTitle.style.backgroundImage = 'url("' + data.image + '")';
   cardTitle.style.backgroundSize = 'cover';
-  cardTitle.style.height = '180px';
   cardWrapper.appendChild(cardTitle);
   let cardTitleTextElement = document.createElement('h2');
   cardTitleTextElement.style.color = '#fff';
