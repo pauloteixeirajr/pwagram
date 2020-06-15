11a973e257a682eb9090f7790ef1d2382dffd2d7
Paulo Teixeira <pauloteixeirajr@outlook.com>
2020-06-15 20:30:21 +0100

    requesting user permission to display notifications

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,4 +1,7 @@
 let deferredPrompt;
+let enableNotificationsButtons = document.querySelectorAll(
+  '.enable-notifications'
+);
 
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('/service-worker.js').then(() => {
@@ -11,3 +14,20 @@ window.addEventListener('beforeinstallprompt', function (event) {
   deferredPrompt = event;
   return false;
 });
+
+function askForNotificationPermissions() {
+  Notification.requestPermission().then(function (result) {
+    if (result !== 'granted') {
+      console.log('No notifications permissions granted!');
+    } else {
+      console.log('User granted permission');
+    }
+  });
+}
+
+if ('Notification' in window) {
+  enableNotificationsButtons.forEach(function (button) {
+    button.style.display = 'inline-block';
+    button.addEventListener('click', askForNotificationPermissions);
+  });
+}
