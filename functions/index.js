e20ed9c681c9495adef5df0263ee22838f4389e3
Paulo Teixeira <pauloteixeirajr@outlook.com>
2020-06-16 19:49:04 +0100

    sending push messages from the server

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,7 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const cors = require('cors')({ origin: true });
+const webpush = require('web-push');
 const serviceAccount = require('./pwgram-private-key.json');
 
 // // Create and Deploy Your First Cloud Functions
@@ -24,7 +25,36 @@ exports.storePostData = functions.https.onRequest((request, response) => {
         image,
       })
       .then(function () {
-        return response.status(201).json({ message: 'Data store', id });
+        webpush.setVapidDetails(
+          'mailto:business@pwagram.com',
+          'BEsuJVLJ6LzSk1Vmf7sL84dIaojqn16MjG_ZPawgsbc1HZJ7UH2gpvuvczyKzrecI5pBC8kkvDIjS7WE10rVfcM',
+          functions.config().pwgram.private_key
+        );
+        return admin
+          .database()
+          .ref('subscriptions')
+          .once('value')
+          .then(function (subscriptions) {
+            subscriptions.forEach(function (sub) {
+              const pushConfig = {
+                endpoint: sub.val().endpoint,
+                keys: {
+                  auth: sub.val().keys.auth,
+                  p256dh: sub.val().keys.p256dh,
+                },
+              };
+              webpush
+                .sendNotification(
+                  pushConfig,
+                  JSON.stringify({
+                    title: 'Your New Post',
+                    content: 'New post added!',
+                  })
+                )
+                .catch(console.log);
+            });
+            return response.status(201).json({ message: 'Data store', id });
+          });
       })
       .catch(function (err) {
         return response.status(500).json({ error: err });
